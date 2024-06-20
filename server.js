9fd07e369568d2f33ed2959300303f7f9fb9568c
Igor Machado Ferrari <igor.mferrari@hotmail.com>
2024-06-20 14:54:35 -0300

    Update frontend

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const app = express()
-
 app.use(bodyParser.json())
+
 const api = require('./rotas/')
 app.use('/api', api)
-const PORT = 3080
 
-console.log(process.env.MONGO_PASS)
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('frontend/build'))
 
-app.get('/',(req, res)=>{
-    res.json({
-        success: true
+    const path = require('path')
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
-})
-
+}
+const PORT = process.env.PORT
 app.listen(PORT)
